c1a42e1236228a249df26b3793e01f0204786880
root <root@vogler.name>
2015-08-20 10:45:45 +0200

    Nuees Script zum aktualsieres des Repo

---
+++ repo_aktualisieren.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Author: L0RE
+# This Script Generates a new Inventory for a new Version or New Plugin
+REPO=/www/repo/gitnew
+cd $REPO/addons
+echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>' >$REPO/addons.xml
+echo '<addons>' >> $REPO/addons.xml
+for name in *; do 
+   VERSION=`cat $name/addon.xml|grep \<addon|grep $name |sed 's/.*version="\([^"]*\)"*.*/\1/g'`
+     if [ ! -f "$name/$name-$VERSION.zip" ]; then
+       zip -r $name/$name-$VERSION.zip $name -x \*.zip
+     fi
+   cat $name/addon.xml|grep -v "<?xml " >> $REPO/addons.xml
+   echo "" >> $REPO/addons.xml
+ done
+ echo "</addons>" >> $REPO/addons.xml
+ md5sum  /www/repo/gitnew/addons.xml >> $REPO/addons.xml.md5
